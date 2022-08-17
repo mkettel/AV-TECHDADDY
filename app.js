899e888f65286478655ab191bc3a71bf81a3cf07
Matthew Kettelkamppwdcd <99507078+mkettel@users.noreply.github.com>
2022-08-16 21:56:29 -0600

    setup js playing around and new contact section

--- app.js
+++ app.js
@@ -16,3 +16,34 @@ document.addEventListener('scroll',()=>{
     }
 })
 
+
+//this is one i took online of how to make result show up after text input. I need to do 
+//a lot more to be able to make it work well but it is a good idea and I could and content like this to a lot of the buttons
+function getUserName() {
+    var nameField = document.getElementById('nameField').value;
+    var result = document.getElementById('result');
+    
+    if (nameField === "Millett") {
+        result.textContent = 'Input Patient information into the Arthrex Tablet';
+        //alert('Millet uses Arthrex shit');
+    } else if (nameField === "Hackett") {
+        result.textContent = "Bring in the S & N Tower and setup that";
+        //alert(nameField);
+    } else {
+        result.textContent = "Look it up fucker";
+    }
+    }
+    var subButton = document.getElementById('subButton');
+    subButton.addEventListener('click', getUserName, false);
+
+//Another function to fuck around with and it works but just dont know how to link to a form 
+
+function doctorName(a) {
+    if (a === 'millett') {
+    return 'Setup the Arthrex Tablet';
+    } else if (a === 'hackett') {
+        return 'Setup the S & N Tower';
+    } else {
+        return 'Go Home Bitch';
+    }
+}
